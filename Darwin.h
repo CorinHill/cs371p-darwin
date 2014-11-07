f268b9f0a0f28bad92042af935667f8321495c09
Corin Hill <corin@cs.utexas.edu>
2014-11-07 00:07:06 -0600

    comments

--- Darwin.h
+++ Darwin.h
@@ -23,7 +23,9 @@ struct Line {
 };
 class World; class Species; class Creature;
 
-
+/**
+ * Describes an instance of Darwin's World and all therein creatures
+ */
 class World {
 private:
   std::vector<int>      _g;
@@ -35,11 +37,38 @@ public:
     _g(x*y), _c(),  _xsize(x), _ysize(y) {
     _c.reserve(20);
   }
-  void addCreature(Creature& c, unsigned x, unsigned y);
+
+  // -------------
+  //  addCreature
+  // -------------
+  /**
+   * Adds creatures to this world at a specified position
+   * @param c Creature&, the creature to be added
+   * @param x int, x position of creature in the world
+   * @param y int, y position of creature in the world
+   */
+  void addCreature(Creature& c, unsigned y, unsigned x);
+  // ---------
+  //   round
+  // ---------
+  /**
+   * Executes one round or time unit in the world
+   * Each creature can act once
+   */
   void round();
+  // --------
+  //   print
+  // --------
+  /**
+   * Prints this world with numeric axis
+   * @param o ostream&, the stream to which will be printed
+   */
   void print(std::ostream& o);
 };
 
+/**
+ * Describes a species behavior and name
+ */
 class Species {
 private:
   std::vector<Line> program;
@@ -47,12 +76,45 @@ public:
   char              name;
   Species(char c = '?') :
     program(), name(c) {}
+  // --------------
+  // addInstruction
+  // --------------
+  /**
+   * Appends a new instruction to this Species program
+   * @param l Line, contains a line of valid creature code
+   */
   void addInstruction(Line l);
+  // --------------
+  // addInstruction
+  // --------------
+  /**
+   * Appends a new instruction to this Species program
+   * @param i Instruction, valid creature code instruction
+   * @param t int, destination if this instruction jumps, otherwise 0
+   */
   void addInstruction(Instruction i, int t);
+  // --------------
+  // seeInstruction
+  // --------------
+  /**
+   * Indexes this species' program
+   * @param i usigned int, the line index
+   * @return the desired line of creature code
+   */
   Line seeInstruction(unsigned i);
+  // -----------
+  //     print
+  // -----------
+  /**
+   * prints this species name (one character)
+   * @param o ostream&, the stream to which will be printed
+   */
   void print(std::ostream& o);
 };
 
+/**
+ * Describes a unique creature
+ */
 class Creature {
 private:
   Species*  _sp;
@@ -66,10 +128,49 @@ public:
     _sp(sp), _pc(0), _dir(dir) {}
   Creature(Species& sp, Direction dir = NORTH) :
     _sp(&sp), _pc(0), _dir(dir) {}
+  // ---------
+  //    act
+  // ---------
+  /**
+   * Based on given information about what is in front of this creature,
+   * Act according to the genetic (species) program
+   * @param ahead int, the type of obstacle of empty
+   * @param other Species*, the species of an enemy if there is one in front
+   * @return the type of action taken
+   */
   int       act(int ahead, Species* other);
+  // ------
+  // facing
+  // ------
+  /**
+   * Described the direction this creature faces
+   * @return the aforementioned description
+   */
   Direction facing() const;
+  // -------
+  // species
+  // -------
+  /**
+   * Desribes the species to which this creature belongs
+   * @return the species
+   */
   Species* species() const;
+  // ------
+  // infect
+  //-------
+  /**
+   * Changes this creatures species to that given, and resets the pc
+   * @param other Species*, the new species of this creature
+   */
   void      infect(Species* other);
+  // -----
+  // print
+  // -----
+  /**
+   * Prints the species name of this creature.
+   * Internally calls Species::print
+   * @param o ostream&, the stream to which will be printed
+   */
   void print(std::ostream& o);
 };
 
